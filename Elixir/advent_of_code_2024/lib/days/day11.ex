6a3b728fefb81f47dcffb9e1ea741993f092f532
Max Doerner <maxdoerner@gmx.net>
2024-12-11 18:40:30 +0100

    Add solution for day 11 of AdventOfCode 2024 in elixir

--- Elixir/advent_of_code_2024/lib/days/day11.ex
+++ Elixir/advent_of_code_2024/lib/days/day11.ex
@@ -2,27 +2,108 @@ defmodule Days.Day11 do
   defstruct []
 
   defimpl DaySolver do
-    @typep day_input() :: any()
+    @typep stone() :: non_neg_integer()
+    @typep day_input() :: {list(stone()), non_neg_integer(), non_neg_integer()}
 
     @spec parse_input(%Days.Day11{}, String.t()) :: day_input()
     def parse_input(_day_solver, input_text) do
-      input_text
+      input_line = hd(String.split(input_text, ~r"\r?\n"))
+      stones = input_line |>
+        String.split(~r"\s+") |>
+        Enum.map(&String.to_integer/1)
+      {stones, 25, 75}
     end
 
 
     @spec solve_part1(%Days.Day11{}, day_input()) :: String.t()
     def solve_part1(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
+      {stones, times, _} = input
+      stones |>
+        blink(times) |>
+        Enum.count() |>
         to_string()
     end
+
+    @spec blink(list(stone), pos_integer()) :: list(stone())
+    defp blink(stones, times) do\
+      Range.new(1, times) |>
+        Enum.reduce(stones, fn _, stones_before_next_blink ->
+          blink_once(stones_before_next_blink)
+        end)
+    end
+
+    @spec blink_once(list(stone)) :: list(stone())
+    defp blink_once(stones) do\
+      stones |>
+        Enum.reduce([], fn stone, processed_stones ->
+          blink_once_single_stone(stone) ++ processed_stones
+        end) |>
+        Enum.reverse()
+    end
+
+    @spec blink_once_single_stone(stone()) :: list(stone())
+    defp blink_once_single_stone(stone) do\
+      if stone == 0 do
+        [1]
+      else
+        stone_text = to_string(stone)
+        digits = String.length(stone_text)
+        if rem(digits, 2) == 0 do
+          stone_text |>
+            String.split_at(floor(digits / 2)) |>
+            Tuple.to_list() |>
+            Enum.map(&String.to_integer/1) |>
+            Enum.reverse()  # we assemble the stones in reverse
+        else
+          [stone * 2024]
+        end
+      end
+    end
+
 
 
     @spec solve_part2(%Days.Day11{}, day_input()) :: String.t()
     def solve_part2(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
+      {stones, _, times} = input
+      stone_count_after_blinking(stones, times) |>
         to_string()
     end
 
+    @spec stone_count_after_blinking(list(stone()), pos_integer()) :: non_neg_integer()
+    defp stone_count_after_blinking(stones, times) do
+      stone_counts = stones |>
+        Enum.reduce(%{}, fn stone, known_stone_counts ->
+          stone_counts_after_blinking(stone, times, known_stone_counts)
+        end)
+      stones |>
+        Enum.map(fn stone -> stone_counts[{stone, times}] end) |>
+        Enum.sum()
+    end
+
+    @spec stone_counts_after_blinking(stone(), non_neg_integer(), %{{stone(), non_neg_integer()} => pos_integer()}) :: %{{stone(), non_neg_integer()} => pos_integer()}
+    defp stone_counts_after_blinking(stone, times, known_stone_counts) do
+      if Map.has_key?(known_stone_counts, {stone, times}) do
+        known_stone_counts
+      else
+        if times == 0 do
+          Map.put(known_stone_counts, {stone, 0}, 1)
+        else
+          after_blink = blink_once_single_stone(stone)
+          new_counts = after_blink |>
+            Enum.reduce(known_stone_counts, fn new_stone, known_counts ->
+              stone_counts_after_blinking(
+                new_stone,
+                times - 1,
+                known_counts
+              )
+            end)
+          stone_count = after_blink |>
+            Enum.map(fn new_stone -> new_counts[{new_stone, times - 1}] end) |>
+            Enum.sum()
+          Map.put(new_counts, {stone, times}, stone_count)
+        end
+      end
+    end
 
   end
 end
--- Elixir/advent_of_code_2024/test/days/day11_test.exs
+++ Elixir/advent_of_code_2024/test/days/day11_test.exs
@@ -6,8 +6,8 @@ defmodule Day11Test do
   end
 
   test "day 11 parsing works for sample input", context do
-    input_string = ~s""
-    expected_input = input_string
+    input_string = ~s"125 17"
+    expected_input = {[125, 17], 25, 75}
 
     solver = AdventOfCode2024.day_solver(context[:day])
     {execution_time, actual_input} = :timer.tc(&DaySolver.parse_input/2, [solver, input_string])
@@ -16,8 +16,8 @@ defmodule Day11Test do
   end
 
   test "day 11 part 1 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"125 17"
+    expected_result = "55312"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
@@ -29,7 +29,7 @@ defmodule Day11Test do
   @tag timeout: :infinity
   test "day 11 part 1 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "203953"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
@@ -39,11 +39,12 @@ defmodule Day11Test do
   end
 
   test "day 11 part 2 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"125 17"
+    expected_result = "55312"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
+    {stones, _, _} = DaySolver.parse_input(solver, input_string)
+    input = {stones, 25, 25} # Special change because there is no actual test example
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time in microseconds")
     assert actual_result == expected_result
@@ -52,7 +53,7 @@ defmodule Day11Test do
   @tag timeout: :infinity
   test "day 11 part 2 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "242090118578155"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
