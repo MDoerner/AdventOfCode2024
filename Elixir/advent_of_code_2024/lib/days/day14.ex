4ccfbcb57374e6fd6a1890d209c896a644266930
Max Doerner <maxdoerner@gmx.net>
2024-12-14 17:41:44 +0100

    Add solution for part 1 of AdventOfCode 2024 in elixir

--- Elixir/advent_of_code_2024/lib/days/day14.ex
+++ Elixir/advent_of_code_2024/lib/days/day14.ex
@@ -2,25 +2,113 @@ defmodule Days.Day14 do
   defstruct []
 
   defimpl DaySolver do
-    @typep day_input() :: any()
+    @typep height() :: pos_integer()
+    @typep width() :: pos_integer()
+    @typep map_dimensions() :: {width(), height()}
+    @typep position() :: {non_neg_integer(), non_neg_integer()}
+    @typep velocity() :: {non_neg_integer(), non_neg_integer()}
+    @typep robot() :: {position(), velocity()}
+    @typep day_input() :: {map_dimensions(), list(robot())}
 
     @spec parse_input(%Days.Day14{}, String.t()) :: day_input()
     def parse_input(_day_solver, input_text) do
-      input_text
+      robots = input_text |>
+        String.split(~r"\r?\n") |>
+        Stream.reject(fn line -> String.match?(line, ~r"^\s*$") end) |>
+        Enum.map(&parse_robot/1)
+      {{101, 103}, robots}
+    end
+
+    @spec parse_robot(String.t()) :: robot()
+    defp parse_robot(text) do
+      [p_x, p_y, v_x, v_y] = Regex.run(
+        ~r"p=(-?\d+),(-?\d+)\s* v=(-?\d+),(-?\d+)",
+        text,
+        capture: :all_but_first
+      ) |>
+        Enum.map(&String.to_integer/1)
+      {{p_x, p_y}, {v_x, v_y}}
     end
 
 
     @spec solve_part1(%Days.Day14{}, day_input()) :: String.t()
     def solve_part1(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
+      {dimensions, robots} = input
+      robots |>
+        Enum.map(fn robot -> robot_position(robot, 100, dimensions) end) |>
+        safety_factor(dimensions) |>
         to_string()
+    end
+
+    @spec robot_position(robot(), integer(), map_dimensions()) :: position()
+    defp robot_position(robot, time, dimensions) do
+      {width, height} = dimensions
+      {{p_x, p_y}, {v_x, v_y}} = robot
+      {mod(p_x + time * v_x, width), mod(p_y + time * v_y, height)}
+    end
+
+    defp mod(a,b) do
+      candiate = rem(a, b)
+      if candiate >= 0 do
+        candiate
+      else
+        candiate + b
+      end
+    end
+
+    @spec safety_factor(list(position()), map_dimensions()) :: non_neg_integer()
+    defp safety_factor(robots, dimensions) do
+      quadrant_counts(robots, dimensions) |>
+        IO.inspect() |>
+        Tuple.to_list() |>
+        Enum.product()
+    end
+
+    @spec quadrant_counts(list(position()), map_dimensions()) :: {non_neg_integer(), non_neg_integer(), non_neg_integer(), non_neg_integer()}
+    defp quadrant_counts(points, dimensions) do
+      {width, height} = dimensions
+      points |>
+        Enum.reduce({0, 0, 0, 0}, fn {x, y}, {c_ul, c_ur, c_ll, c_lr} ->
+          cond do
+            x < floor(width/2) and y < floor(height/2) -> {c_ul + 1, c_ur, c_ll, c_lr}
+            x > floor(width/2) and y < floor(height/2) -> {c_ul, c_ur + 1, c_ll, c_lr}
+            x < floor(width/2) and y > floor(height/2) -> {c_ul, c_ur, c_ll + 1, c_lr}
+            x > floor(width/2) and y > floor(height/2) -> {c_ul, c_ur, c_ll, c_lr + 1}
+            true -> {c_ul, c_ur, c_ll, c_lr}
+          end
+        end)
+
     end
 
 
     @spec solve_part2(%Days.Day14{}, day_input()) :: String.t()
     def solve_part2(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
-        to_string()
+      {dimensions, robots} = input
+      robots |>
+        Enum.map(fn robot -> robot_position(robot, 10, dimensions) end) |>
+        print_positions(dimensions)
+    end
+
+    @spec print_positions(list(position()), map_dimensions()) :: String.t()
+    defp print_positions(points, dimensions) do
+      {width, height} = dimensions
+      point_map = Enum.frequencies(points)
+      x_range = Range.new(0, width - 1)
+      y_range = Range.new(0, height - 1)
+      y_range |>
+        Enum.map(fn y ->
+          x_range |>
+            Enum.map(fn x ->
+              if Map.has_key?(point_map, {x, y}) do
+                Map.get(point_map, {x, y}, 0) |> to_string()
+              else
+                " "
+              end
+            end) |>
+            Enum.join() |>
+            IO.inspect()
+        end) |>
+        Enum.join("\r\n")
     end
 
 
--- Elixir/advent_of_code_2024/test/days/day14_test.exs
+++ Elixir/advent_of_code_2024/test/days/day14_test.exs
@@ -6,8 +6,35 @@ defmodule Day14Test do
   end
 
   test "day 14 parsing works for sample input", context do
-    input_string = ~s""
-    expected_input = input_string
+    input_string = ~s"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"
+    expected_input = {
+      {101, 103},
+      [
+        {{0, 4}, {3, -3}},
+        {{6, 3}, {-1, -3}},
+        {{10, 3}, {-1, 2}},
+        {{2, 0}, {2, -1}},
+        {{0, 0}, {1, 3}},
+        {{3, 0}, {-2, -2}},
+        {{7, 6}, {-1, -3}},
+        {{3, 0}, {-1, -2}},
+        {{9, 3}, {2, 3}},
+        {{7, 3}, {-1, 2}},
+        {{2, 4}, {2, -3}},
+        {{9, 5}, {-3, -3}}
+      ]
+    }
 
     solver = AdventOfCode2024.day_solver(context[:day])
     {execution_time, actual_input} = :timer.tc(&DaySolver.parse_input/2, [solver, input_string])
@@ -16,11 +43,23 @@ defmodule Day14Test do
   end
 
   test "day 14 part 1 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"p=0,4 v=3,-3
+p=6,3 v=-1,-3
+p=10,3 v=-1,2
+p=2,0 v=2,-1
+p=0,0 v=1,3
+p=3,0 v=-2,-2
+p=7,6 v=-1,-3
+p=3,0 v=-1,-2
+p=9,3 v=2,3
+p=7,3 v=-1,2
+p=2,4 v=2,-3
+p=9,5 v=-3,-3"
+    expected_result = "12"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
+    {_, robots} = DaySolver.parse_input(solver, input_string)
+    input = {{11, 7}, robots}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time in microseconds")
     assert actual_result == expected_result
@@ -29,7 +68,7 @@ defmodule Day14Test do
   @tag timeout: :infinity
   test "day 14 part 1 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "226236192"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
@@ -38,16 +77,6 @@ defmodule Day14Test do
     assert actual_result == expected_result
   end
 
-  test "day 14 part 2 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
-
-    solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
-    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
-    IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time in microseconds")
-    assert actual_result == expected_result
-  end
 
   @tag timeout: :infinity
   test "day 14 part 2 works for real input", context do
