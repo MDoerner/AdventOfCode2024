b0fc6b229843539a09f4e3bb77349b72ff14b2d6
Max Doerner <maxdoerner@gmx.net>
2024-12-18 14:22:46 +0100

    Add solution for day 18 of AdventOfCode 2024 in elixir

--- Elixir/advent_of_code_2024/lib/days/day18.ex
+++ Elixir/advent_of_code_2024/lib/days/day18.ex
@@ -2,27 +2,188 @@ defmodule Days.Day18 do
   defstruct []
 
   defimpl DaySolver do
-    @typep day_input() :: any()
+    @typep height() :: pos_integer()
+    @typep width() :: pos_integer()
+    @typep map_dimensions() :: {width(), height()}
+    @typep position() :: {non_neg_integer(), non_neg_integer()}
+    @typep direction() :: {1, 0} | {0, 1} | {-1, 0} | {0, -1}
+    @typep block() :: position()
+    @typep corruptions() :: MapSet.t(position())
+    @typep start_point() :: position()
+    @typep end_point() :: position()
+    @typep day_input() :: {start_point(), end_point(), map_dimensions(), list(block()), non_neg_integer()}
 
     @spec parse_input(%Days.Day18{}, String.t()) :: day_input()
     def parse_input(_day_solver, input_text) do
-      input_text
+      blocks = input_text |>
+        String.split(~r"\r?\n") |>
+        Stream.reject(fn line -> String.match?(line, ~r"^\s*$") end) |>
+        Enum.map(fn line ->
+          line |>
+            String.split(~r",") |>
+            Enum.map(&String.to_integer/1) |>
+            List.to_tuple()
+        end)
+        {{0, 0}, {70, 70}, {71, 71}, blocks, 1024}
     end
 
 
     @spec solve_part1(%Days.Day18{}, day_input()) :: String.t()
     def solve_part1(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
+      {start_point, end_point, dimensions, blocks, blocks_fallen} = input
+      corruptions = blocks |>
+        Enum.take(blocks_fallen) |>
+        Enum.into(MapSet.new())
+      shortest_distance(start_point, end_point, dimensions, corruptions) |>
         to_string()
     end
+
+    @spec shortest_distance(start_point(), end_point(), map_dimensions(), corruptions()) :: integer()
+    defp shortest_distance(start_point, end_point, dimensions, corruptions) do
+      distances_to_start(
+        end_point,
+        dimensions,
+        corruptions,
+        %{},
+        Heap.new(fn {distance_1, _}, {distance_2, _} -> distance_1 <= distance_2 end) |>
+          Heap.push({0, start_point})
+      )
+    end
+
+
+    @spec distances_to_start(end_point(), map_dimensions(), corruptions(), %{position() => non_neg_integer()}, Heap.t()) :: integer()
+    defp distances_to_start(end_point, dimensions, corruptions, known_distances, interesting_points) do
+      if Heap.empty?(interesting_points) do
+        # The end point is not reachable.
+        -1
+      else
+        {distance, point} = Heap.root(interesting_points)
+        if point == end_point do
+          distance
+        else
+          remaining_heap = Heap.pop(interesting_points)
+          if Map.has_key?(known_distances, point) do
+            # We have found this point and direction already with lower distance.
+            # We need to check this because we do not delete old versions on update of distance.
+            distances_to_start(end_point, dimensions, corruptions, known_distances, remaining_heap)
+          else
+            new_known = Map.put(known_distances, point, distance)
+            accessible_neighbours = neighbours(point) |>
+              Enum.filter(fn neighbour ->
+                on_map?(neighbour, dimensions)
+                  and not MapSet.member?(corruptions, neighbour)
+                  and not Map.has_key?(known_distances, neighbour)
+              end)
+            new_heap = accessible_neighbours |>
+              Enum.reduce(remaining_heap, fn neighbour, heap ->
+                Heap.push(heap, {distance + 1, neighbour})
+              end)
+            distances_to_start(end_point, dimensions, corruptions, new_known, new_heap)
+          end
+        end
+      end
+    end
+
+    @spec neighbours(position()) :: list(position())
+    defp neighbours(point) do
+      [{1, 0}, {0, 1}, {-1, 0}, {0, -1}] |>
+        Enum.map(fn direction -> neighbour_in_direction(point, direction) end)
+    end
+
+    @spec neighbour_in_direction(position(), direction()) :: position()
+    defp neighbour_in_direction(point, direction) do
+      {y, x} = point
+      {y_d, x_d} = direction
+      {y + y_d, x + x_d}
+    end
+
+    @spec on_map?(position(), map_dimensions()) :: boolean()
+    defp on_map?(point, dimensions) do
+      {y, x} = point
+      {height, width} = dimensions
+      0 <= y and 0 <= x and y < height and x < width
+    end
+
 
 
     @spec solve_part2(%Days.Day18{}, day_input()) :: String.t()
     def solve_part2(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
-        to_string()
+      {start_point, end_point, dimensions, blocks, known_non_blocking_block_count} = input
+      first_blocking_block(start_point, end_point, dimensions, blocks, known_non_blocking_block_count) |>
+        Tuple.to_list() |>
+        Enum.join(",")
     end
 
+    @spec first_blocking_block(start_point(), end_point(), map_dimensions(), list(block()), non_neg_integer()) :: block() | nil
+    defp first_blocking_block(start_point, end_point, dimensions, blocks, known_safe_block_count) do
+      if end_point_reachable?(start_point, end_point, dimensions, blocks |> Enum.into(MapSet.new())) do
+        # The path will never be blocked.
+        nil
+      else
+        start_corruptions = blocks |>
+          Enum.take(known_safe_block_count) |>
+          Enum.into(MapSet.new())
+        remaining_blocks = blocks |>
+          Enum.drop(known_safe_block_count)
+        first_blocking_block_impl(start_point, end_point, dimensions, remaining_blocks, start_corruptions)
+      end
+    end
+
+    @spec first_blocking_block_impl(start_point(), end_point(), map_dimensions(), list(block()), corruptions()) :: block() | nil
+    defp first_blocking_block_impl(start_point, end_point, dimensions, remaining_blocks, known_corruptions) do
+      block_count = length(remaining_blocks)
+      case block_count do
+        1 -> hd(remaining_blocks)
+        0 -> nil
+        _ ->
+          middle = floor(block_count / 2)
+          corruptions = remaining_blocks |>
+            Enum.take(middle) |>
+            Enum.into(known_corruptions)
+          if end_point_reachable?(start_point, end_point, dimensions, corruptions) do
+            first_blocking_block_impl(
+              start_point,
+              end_point,
+              dimensions,
+              remaining_blocks |> Enum.drop(middle),
+              corruptions
+            )
+          else
+            first_blocking_block_impl(
+              start_point,
+              end_point,
+              dimensions,
+              remaining_blocks |> Enum.take(middle),
+              known_corruptions
+            )
+          end
+      end
+    end
+
+    @spec end_point_reachable?(position(), position(), map_dimensions(), corruptions()) :: boolean()
+    defp end_point_reachable?(start_point, end_point, dimensions, corruptions) do
+      0 <= shortest_distance(start_point, end_point, dimensions, corruptions)
+    end
+
+    # Returns the first blocking block, if there is any, and all corruptions, otherwise.
+    # Sufficiently fast, but faster is easily possible
+    @spec first_blocking_block_naive(start_point(), end_point(), map_dimensions(), list(block()), non_neg_integer()) :: block() | corruptions()
+    defp first_blocking_block_naive(start_point, end_point, dimensions, blocks, known_safe_block_count) do
+      start_corruptions = blocks |>
+        Enum.take(known_safe_block_count) |>
+        Enum.into(MapSet.new())
+      remaining_blocks = blocks |>
+        Enum.drop(known_safe_block_count)
+      remaining_blocks |>
+        Enum.reduce_while(start_corruptions, fn block, corruptions ->
+          new_corruptions = MapSet.put(corruptions, block)
+          if end_point_reachable?(start_point, end_point, dimensions, new_corruptions) do
+            {:cont, new_corruptions}
+          else
+            {:halt, block}
+          end
+        end)
+    end
 
   end
 end
--- Elixir/advent_of_code_2024/test/days/day18_test.exs
+++ Elixir/advent_of_code_2024/test/days/day18_test.exs
@@ -6,8 +6,38 @@ defmodule Day18Test do
   end
 
   test "day 18 parsing works for sample input", context do
-    input_string = ~s""
-    expected_input = input_string
+    input_string = ~s"5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"
+    expected_input = {
+      {0, 0},
+      {70, 70},
+      {71, 71},
+      [{5,4}, {4,2}, {4,5}, {3,0}, {2,1}, {6,3}, {2,4}, {1,5}, {0,6}, {3,3}, {2,6}, {5,1}, {1,2}, {5,5}, {2,5}, {6,5}, {1,4}, {0,4}, {6,4}, {1,1}, {6,1}, {1,0}, {0,5}, {1,6}, {2,0}],
+      1024
+    }
 
     solver = AdventOfCode2024.day_solver(context[:day])
     {execution_time, actual_input} = :timer.tc(&DaySolver.parse_input/2, [solver, input_string])
@@ -16,11 +46,36 @@ defmodule Day18Test do
   end
 
   test "day 18 part 1 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"
+    expected_result = "22"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
+    {_, _, _, blocks, _} = DaySolver.parse_input(solver, input_string)
+    input = {{0, 0}, {6, 6}, {7, 7}, blocks, 12}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time in microseconds")
     assert actual_result == expected_result
@@ -29,7 +84,7 @@ defmodule Day18Test do
   @tag timeout: :infinity
   test "day 18 part 1 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "306"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
@@ -39,11 +94,36 @@ defmodule Day18Test do
   end
 
   test "day 18 part 2 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"5,4
+4,2
+4,5
+3,0
+2,1
+6,3
+2,4
+1,5
+0,6
+3,3
+2,6
+5,1
+1,2
+5,5
+2,5
+6,5
+1,4
+0,4
+6,4
+1,1
+6,1
+1,0
+0,5
+1,6
+2,0"
+    expected_result = "6,1"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
+    {_, _, _, blocks, _} = DaySolver.parse_input(solver, input_string)
+    input = {{0, 0}, {6, 6}, {7, 7}, blocks, 12}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time in microseconds")
     assert actual_result == expected_result
@@ -52,7 +132,7 @@ defmodule Day18Test do
   @tag timeout: :infinity
   test "day 18 part 2 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "38,63"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
