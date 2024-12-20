210406fe0353ac89153aa0f2e994c60c0296d30d
Max Doerner <maxdoerner@gmx.net>
2024-12-20 10:52:30 +0100

    Add solution for part 1 of day 20 of AdventOfCode 2024 in elixir

--- Elixir/advent_of_code_2024/lib/days/day20.ex
+++ Elixir/advent_of_code_2024/lib/days/day20.ex
@@ -2,18 +2,123 @@ defmodule Days.Day20 do
   defstruct []
 
   defimpl DaySolver do
-    @typep day_input() :: any()
+    @typep position() :: {non_neg_integer(), non_neg_integer()}
+    @typep direction() :: {1, 0} | {0, 1} | {-1, 0} | {0, -1}
+    @typep corridor() :: position()
+    @typep track() :: MapSet.t(corridor())
+    @typep start_point() :: position()
+    @typep end_point() :: position()
+    @typep shortcut_goal() :: integer()
+    @typep day_input() :: {start_point(), end_point(), track(), shortcut_goal()}
+    @typep shortcut() :: {position(), position()}
 
     @spec parse_input(%Days.Day20{}, String.t()) :: day_input()
     def parse_input(_day_solver, input_text) do
-      input_text
+      {start_point, end_point, corridors} = input_text |>
+        String.split(~r"\r?\n") |>
+        Stream.reject(fn line -> String.match?(line, ~r"^\s*$") end) |>
+        Stream.with_index() |>
+        Enum.reduce({nil, nil, MapSet.new()}, fn {row, y}, row_maze ->
+          row |>
+            String.graphemes() |>
+            Stream.with_index() |>
+            Enum.reduce(row_maze, fn {tile, x}, {start_point, end_point, corridors} ->
+              case tile do
+                "S" -> {{y, x}, end_point, MapSet.put(corridors, {y, x})}
+                "E" -> {start_point, {y, x}, MapSet.put(corridors, {y, x})}
+                "." -> {start_point, end_point, MapSet.put(corridors, {y, x})}
+                _ -> {start_point, end_point, corridors}
+              end
+            end)
+        end)
+      {start_point, end_point, corridors, 100}
     end
 
 
     @spec solve_part1(%Days.Day20{}, day_input()) :: String.t()
     def solve_part1(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
+      {start_point, end_point, track, shortcut_goal} = input
+      short_shortcuts_above_threshold(start_point, end_point, track, shortcut_goal) |>
+        Enum.count() |>
         to_string()
+    end
+
+    @spec short_shortcuts_above_threshold(start_point(), end_point(), track(), non_neg_integer()) :: MapSet.t(shortcut())
+    defp short_shortcuts_above_threshold(start_point, end_point, track, threshold) do
+      distances_to_goal = track_goal_distances(start_point, end_point, track)
+      track |>
+        Enum.reduce(MapSet.new(), fn start, known_shortcuts ->
+          neighbours(start) |>
+            Enum.reject(fn neighbour -> MapSet.member?(track, neighbour) end) |>
+            Enum.reduce(known_shortcuts, fn first, known_shorts_first ->
+              immediate_exists = neighbours(first) |>
+                Enum.filter(fn next_neighbour ->
+                  MapSet.member?(track, next_neighbour)
+                    and next_neighbour != start
+                end)
+              immediate_exists |>
+                Enum.reduce(known_shorts_first, fn immediate_exit, known ->
+                  safe = shortcut_safe(start, immediate_exit, distances_to_goal)
+                  if safe >= threshold do
+                    MapSet.put(known, {start, immediate_exit})
+                  else
+                    known
+                  end
+                end)
+            end)
+        end)
+    end
+
+
+    @spec track_goal_distances(start_point(), end_point(), track()) :: %{corridor() => non_neg_integer()}
+    defp track_goal_distances(start_point, end_point, track) do
+      track_goal_distances_impl(end_point, 0, start_point, track, %{})
+    end
+
+    @spec track_goal_distances_impl(position(), non_neg_integer(), start_point(), track(), %{corridor() => non_neg_integer()}) :: %{corridor() => non_neg_integer()}
+    defp track_goal_distances_impl(point, distance, start_point, track, known_distances) do
+      new_known = Map.put(known_distances, point, distance)
+      if point == start_point do
+        new_known
+      else
+        next_point = neighbours(point) |>
+          Enum.filter(fn neighbour ->
+            MapSet.member?(track, neighbour)
+              and not Map.has_key?(known_distances, neighbour)
+          end) |>
+          List.first()
+        track_goal_distances_impl(next_point, distance + 1, start_point, track, new_known)
+      end
+    end
+
+    @spec neighbours(position()) :: list(position())
+    defp neighbours(point) do
+      [{1, 0}, {0, 1}, {-1, 0}, {0, -1}] |>
+        Enum.map(fn direction -> neighbour_in_direction(point, direction) end)
+    end
+
+    @spec neighbour_in_direction(position(), direction()) :: position()
+    defp neighbour_in_direction(point, direction) do
+      {y, x} = point
+      {y_d, x_d} = direction
+      {y + y_d, x + x_d}
+    end
+
+    @spec shortcut_safe(position(), position(), %{position() => non_neg_integer()}) :: integer()
+    defp shortcut_safe(from_point, to_point, distances_to_goal) do
+      track_distance(from_point, to_point, distances_to_goal) - manhatten_distance(from_point, to_point)
+    end
+
+    @spec track_distance(position(), position(), %{position() => non_neg_integer()}) :: integer()
+    defp track_distance(from_point, to_point, distances_to_goal) do
+      distances_to_goal[to_point] - distances_to_goal[from_point]
+    end
+
+    @spec manhatten_distance(position(), position()) :: non_neg_integer()
+    defp manhatten_distance(point, other_point) do
+      {x_1, y_1} = point
+      {x_2, y_2} = other_point
+      abs(x_1 - x_2) + abs(y_1 - y_2)
     end
 
 
@@ -22,6 +127,7 @@ defmodule Days.Day20 do
       (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
         to_string()
     end
+
 
 
   end
--- Elixir/advent_of_code_2024/test/days/day20_test.exs
+++ Elixir/advent_of_code_2024/test/days/day20_test.exs
@@ -5,22 +5,27 @@ defmodule Day20Test do
     {:ok, %{day: 20}}
   end
 
-  test "day 20 parsing works for sample input", context do
-    input_string = ~s""
-    expected_input = input_string
-
-    solver = AdventOfCode2024.day_solver(context[:day])
-    {execution_time, actual_input} = :timer.tc(&DaySolver.parse_input/2, [solver, input_string])
-    IO.inspect(execution_time, label: "day #{context[:day]} example parsing execution time in microseconds")
-    assert actual_input == expected_input
-  end
-
   test "day 20 part 1 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############"
+    expected_result = "8"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
+    {start_point, end_point, track, _goal} = DaySolver.parse_input(solver, input_string)
+    input = {start_point, end_point, track, 12}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time in microseconds")
     assert actual_result == expected_result
@@ -29,7 +34,7 @@ defmodule Day20Test do
   @tag timeout: :infinity
   test "day 20 part 1 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "1286"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
@@ -39,11 +44,26 @@ defmodule Day20Test do
   end
 
   test "day 20 part 2 works for sample input", context do
-    input_string = ~s""
-    expected_result = "42"
+    input_string = ~s"###############
+#...#...#.....#
+#.#.#.#.#.###.#
+#S#...#.#.#...#
+#######.#.#.###
+#######.#.#...#
+#######.#.###.#
+###..E#...#...#
+###.#######.###
+#...###...#...#
+#.#####.#.###.#
+#.#...#.#.#...#
+#.#.#.#.#.#.###
+#...#...#...###
+###############"
+    expected_result = "259"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    input = DaySolver.parse_input(solver, input_string)
+    {start_point, end_point, track, _goal} = DaySolver.parse_input(solver, input_string)
+    input = {start_point, end_point, track, 50}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time in microseconds")
     assert actual_result == expected_result
