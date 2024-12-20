7c1d66d023c366594145e2945e0ce46c4a4508f6
Max Doerner <maxdoerner@gmx.net>
2024-12-20 14:54:56 +0100

    Add solution for part 2 of day 20 of AdventOfCode 2024 in elixir

--- Elixir/advent_of_code_2024/lib/days/day20.ex
+++ Elixir/advent_of_code_2024/lib/days/day20.ex
@@ -2,6 +2,9 @@ defmodule Days.Day20 do
   defstruct []
 
   defimpl DaySolver do
+    @typep height() :: pos_integer()
+    @typep width() :: pos_integer()
+    @typep map_dimensions() :: {width(), height()}
     @typep position() :: {non_neg_integer(), non_neg_integer()}
     @typep direction() :: {1, 0} | {0, 1} | {-1, 0} | {0, -1}
     @typep corridor() :: position()
@@ -9,14 +12,17 @@ defmodule Days.Day20 do
     @typep start_point() :: position()
     @typep end_point() :: position()
     @typep shortcut_goal() :: integer()
-    @typep day_input() :: {start_point(), end_point(), track(), shortcut_goal()}
+    @typep day_input() :: {start_point(), end_point(), track(), map_dimensions(), shortcut_goal()}
     @typep shortcut() :: {position(), position()}
 
     @spec parse_input(%Days.Day20{}, String.t()) :: day_input()
     def parse_input(_day_solver, input_text) do
-      {start_point, end_point, corridors} = input_text |>
+      lines = input_text |>
         String.split(~r"\r?\n") |>
-        Stream.reject(fn line -> String.match?(line, ~r"^\s*$") end) |>
+        Enum.reject(fn line -> String.match?(line, ~r"^\s*$") end)
+      height = Enum.count(lines)
+      width = List.first(lines) |> String.length()
+      {start_point, end_point, corridors} = lines |>
         Stream.with_index() |>
         Enum.reduce({nil, nil, MapSet.new()}, fn {row, y}, row_maze ->
           row |>
@@ -31,13 +37,13 @@ defmodule Days.Day20 do
               end
             end)
         end)
-      {start_point, end_point, corridors, 100}
+      {start_point, end_point, corridors, {height, width}, 100}
     end
 
 
     @spec solve_part1(%Days.Day20{}, day_input()) :: String.t()
     def solve_part1(_day_solver, input) do
-      {start_point, end_point, track, shortcut_goal} = input
+      {start_point, end_point, track, _dimensions, shortcut_goal} = input
       short_shortcuts_above_threshold(start_point, end_point, track, shortcut_goal) |>
         Enum.count() |>
         to_string()
@@ -58,7 +64,7 @@ defmodule Days.Day20 do
                 end)
               immediate_exists |>
                 Enum.reduce(known_shorts_first, fn immediate_exit, known ->
-                  safe = shortcut_safe(start, immediate_exit, distances_to_goal)
+                  safe = short_shortcut_save(start, immediate_exit, distances_to_goal)
                   if safe >= threshold do
                     MapSet.put(known, {start, immediate_exit})
                   else
@@ -104,8 +110,8 @@ defmodule Days.Day20 do
       {y + y_d, x + x_d}
     end
 
-    @spec shortcut_safe(position(), position(), %{position() => non_neg_integer()}) :: integer()
-    defp shortcut_safe(from_point, to_point, distances_to_goal) do
+    @spec short_shortcut_save(position(), position(), %{position() => non_neg_integer()}) :: integer()
+    defp short_shortcut_save(from_point, to_point, distances_to_goal) do
       track_distance(from_point, to_point, distances_to_goal) - manhatten_distance(from_point, to_point)
     end
 
@@ -124,9 +130,114 @@ defmodule Days.Day20 do
 
     @spec solve_part2(%Days.Day20{}, day_input()) :: String.t()
     def solve_part2(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
+      {start_point, end_point, track, _dimensions, shortcut_goal} = input
+      shortcuts_above_threshold(start_point, end_point, track, shortcut_goal, 20) |>
+        Enum.count() |>
         to_string()
     end
+
+    @spec shortcuts_above_threshold(start_point(), end_point(), track(), non_neg_integer(), pos_integer()) :: MapSet.t(shortcut())
+    defp shortcuts_above_threshold(start_point, end_point, track, threshold, max_shortcut_length) do
+      track_distances = track_goal_distances(start_point, end_point, track)
+      track |>
+        Enum.reduce(MapSet.new(), fn shortcut_start, shortcuts ->
+          track |>
+            Enum.filter(fn shortcut_end ->
+              manhatten_distance(shortcut_start, shortcut_end) <= max_shortcut_length
+                and short_shortcut_save(shortcut_start, shortcut_end, track_distances) >= threshold
+            end) |>
+            Enum.map(fn shortcut_end -> {shortcut_start, shortcut_end} end) |>
+            Enum.into(shortcuts)
+        end)
+    end
+
+    # The below was based on the wrong assumption that a cheat needs to stay off-road, which makes the problem actually quite a bit harder.
+
+    # @spec shortcuts_above_threshold(start_point(), end_point(), track(), map_dimensions(), non_neg_integer(), pos_integer()) :: MapSet.t(shortcut())
+    # defp shortcuts_above_threshold(start_point, end_point, track, dimensions, threshold, max_shortcut_length) do
+    #   track_distances = track_goal_distances(start_point, end_point, track)
+    #   track |>
+    #     Enum.reduce(MapSet.new(), fn start, shortcuts ->
+    #       points_within_off_road_distance(start, max_shortcut_length, track, dimensions) |>
+    #         Enum.filter(fn {shortcut_end, shortcut_length} ->
+    #           shortcut_saved_distance(start, shortcut_end, shortcut_length, track_distances) >= threshold
+    #         end) |>
+    #         Enum.map(fn {shortcut_end, _shortcut_length} -> {start, shortcut_end} end) |>
+    #         Enum.into(shortcuts)
+    #     end)
+    # end
+
+    # @spec points_within_off_road_distance(position(), non_neg_integer(), track(), map_dimensions()) :: %{corridor() => non_neg_integer()}
+    # defp points_within_off_road_distance(start_point, max_distance, track, dimensions) do
+    #   first_off_road_neighbours = neighbours(start_point) |>
+    #     Enum.reject(fn neighbour -> MapSet.member?(track, neighbour) end)
+    #   initial_interesting = first_off_road_neighbours |>
+    #     Enum.reduce(Heap.new(fn {distance_1, _}, {distance_2, _} -> distance_1 <= distance_2 end), fn neighbour, interesting ->
+    #       Heap.push(interesting, {1, neighbour})
+    #     end)
+    #   off_road_distance_points(
+    #     max_distance,
+    #     track,
+    #     dimensions,
+    #     MapSet.new(),
+    #     initial_interesting,
+    #     %{}
+    #   )
+    # end
+
+    # @spec off_road_distance_points(non_neg_integer(), track(), map_dimensions(), MapSet.t(position()), Heap.t(), %{corridor() => non_neg_integer()}) :: %{corridor() => non_neg_integer()}
+    # defp off_road_distance_points(max_distance, track, dimensions, visited_off_road_points, interesting_points, known_shortcut_ends) do
+    #   if Heap.empty?(interesting_points) do
+    #     known_shortcut_ends
+    #   else
+    #     {distance, point} = Heap.root(interesting_points)
+    #     if distance >= max_distance do
+    #       known_shortcut_ends
+    #     else
+    #       remaining_heap = Heap.pop(interesting_points)
+    #       if Map.has_key?(visited_off_road_points, point) do
+    #         # We have found this point and direction already with lower distance.
+    #         # We need to check this because we do not delete old versions on update of distance.
+    #         off_road_distance_points(max_distance, track, dimensions, visited_off_road_points, remaining_heap, known_shortcut_ends)
+    #       else
+    #         new_visited = MapSet.put(visited_off_road_points, point)
+    #         new_on_road_neighbours = neighbours(point) |>
+    #           Enum.filter(fn neighbour ->
+    #             MapSet.member?(track, neighbour)
+    #               and not Map.has_key?(known_shortcut_ends, neighbour)
+    #           end)
+    #         new_known = new_on_road_neighbours |>
+    #           Enum.reduce(known_shortcut_ends, fn neighbour, known ->
+    #             Map.put(known, neighbour, distance + 1)
+    #           end)
+    #         new_off_road_neighbours = neighbours(point) |>
+    #           Enum.filter(fn neighbour ->
+    #             not MapSet.member?(track, neighbour)
+    #               and not MapSet.member?(visited_off_road_points, neighbour)
+    #               and on_map?(neighbour, dimensions)
+    #           end)
+    #         new_heap = new_off_road_neighbours |>
+    #           Enum.reduce(remaining_heap, fn neighbour, heap ->
+    #             Heap.push(heap, {distance + 1, neighbour})
+    #           end)
+    #         off_road_distance_points(max_distance, track, dimensions, new_visited, new_heap, new_known)
+    #       end
+    #     end
+    #   end
+    # end
+
+    # @spec on_map?(position(), map_dimensions()) :: boolean()
+    # defp on_map?(point, dimensions) do
+    #   {y, x} = point
+    #   {height, width} = dimensions
+    #   0 <= y and 0 <= x and y < height and x < width
+    # end
+
+
+    # @spec shortcut_saved_distance(position(), position(), pos_integer(), %{position() => non_neg_integer()}) :: integer()
+    # defp shortcut_saved_distance(from_point, to_point, shortcut_length, distances_to_goal) do
+    #   track_distance(from_point, to_point, distances_to_goal) - shortcut_length
+    # end
 
 
 
--- Elixir/advent_of_code_2024/test/days/day20_test.exs
+++ Elixir/advent_of_code_2024/test/days/day20_test.exs
@@ -24,8 +24,8 @@ defmodule Day20Test do
     expected_result = "8"
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    {start_point, end_point, track, _goal} = DaySolver.parse_input(solver, input_string)
-    input = {start_point, end_point, track, 12}
+    {start_point, end_point, track, dimensions, _goal} = DaySolver.parse_input(solver, input_string)
+    input = {start_point, end_point, track, dimensions, 12}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time in microseconds")
     assert actual_result == expected_result
@@ -43,6 +43,7 @@ defmodule Day20Test do
     assert actual_result == expected_result
   end
 
+  @tag timeout: 600
   test "day 20 part 2 works for sample input", context do
     input_string = ~s"###############
 #...#...#.....#
@@ -59,11 +60,11 @@ defmodule Day20Test do
 #.#.#.#.#.#.###
 #...#...#...###
 ###############"
-    expected_result = "259"
+    expected_result = (3+4+22+12+14+12+19+20+23+25+39+29+31+32) |> to_string()
 
     solver = AdventOfCode2024.day_solver(context[:day])
-    {start_point, end_point, track, _goal} = DaySolver.parse_input(solver, input_string)
-    input = {start_point, end_point, track, 50}
+    {start_point, end_point, track, dimensions, _goal} = DaySolver.parse_input(solver, input_string)
+    input = {start_point, end_point, track, dimensions, 50}
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time in microseconds")
     assert actual_result == expected_result
@@ -72,7 +73,7 @@ defmodule Day20Test do
   @tag timeout: :infinity
   test "day 20 part 2 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "989316"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
