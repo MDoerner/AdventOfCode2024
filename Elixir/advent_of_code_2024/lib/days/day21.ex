1e90b8e7404d4ed6447dccb1fb24dd902868b93a
Max Doerner <maxdoerner@gmx.net>
2024-12-22 00:46:55 +0100

    Add solution for day 21 of AdventOfCode 2024 in elixir

--- Elixir/advent_of_code_2024/lib/days/day21.ex
+++ Elixir/advent_of_code_2024/lib/days/day21.ex
@@ -30,21 +30,19 @@ defmodule Days.Day21 do
       else
         String.to_integer(text)
       end
-    end
+  end
 
 
-    @spec solve_part1(%Days.Day21{}, day_input()) :: String.t()
-    def solve_part1(_day_solver, input) do
-      input |>
-        Enum.map(&complexity/1) |>
-        Enum.sum() |>
-        to_string()
-    end
 
-    @spec complexity(code()) :: non_neg_integer()
-    defp complexity(code) do
+
+
+
+
+    @spec complexity(code(), pos_integer()) :: non_neg_integer()
+    defp complexity(code, number_of_robots) do
       number = number_part(code)
-      shortest_length = shortest_sequence_length(code)
+      shortest_length = shortest_sequence_length(code, number_of_robots)
+      number * shortest_length
     end
 
     @spec number_part(code()) :: non_neg_integer()
@@ -55,25 +53,72 @@ defmodule Days.Day21 do
         String.to_integer()
     end
 
-    @spec shortest_sequence_length(code()) :: non_neg_integer()
-    defp shortest_sequence_length(code) do
+    @spec shortest_sequence_length(code(), pos_integer()) :: non_neg_integer()
+    defp shortest_sequence_length(code, number_of_robots) do
+      seqs = shortest_sequences(code, number_of_robots)
+      seqs |> Enum.take(1) |> hd |> Enum.count()
+    end
 
+    @spec shortest_sequences(code(), non_neg_integer()) :: MapSet.t(move_sequence())
+    defp shortest_sequences(code, number_of_robots) do
+        digi_transitions = shortest_sensible_direct_digi_pad_transitions()
+        direction_transitions = shortest_direct_keypad_transitions()
+        first_directional_inputs = shortest_indirect_sequences(code, digi_transitions)
+        if number_of_robots == 0 do
+          first_directional_inputs
+        else
+          Range.new(1, number_of_robots) |>
+          Enum.reduce(first_directional_inputs, fn _, prior_robot_inputs ->
+            prior_robot_inputs |>
+              Enum.reduce(MapSet.new(), fn prior_seq, known_seqs ->
+                shortest_indirect_sequences(prior_seq, direction_transitions) |>
+                  Enum.into(known_seqs)
+              end) |>
+              shortest()
+          end)
+        end
+    end
+
+    @spec shortest_indirect_sequences(move_sequence() | code(), %{{direction_button(), direction_button()} => MapSet.t(move_sequence())} | %{{digi_button(), digi_button()} => MapSet.t(move_sequence())}) :: MapSet.t(move_sequence())
+    defp shortest_indirect_sequences(button_sequence, direct_transitions) do
+      {_, all_sequences} = button_sequence |>
+         Enum.reduce({:a, MapSet.new([[]])}, fn button, {previous_button, possible_starts} ->
+
+           transitions = direct_transitions[{previous_button, button}]
+           sequences = transitions |>
+             Enum.reduce(MapSet.new(), fn transition, know_sequences ->
+               possible_starts |>
+                 Enum.reduce(know_sequences, fn start, known ->
+                   MapSet.put(known, start ++ transition ++ [:a])
+                 end)
+             end)
+           {button, sequences}
+      end)
+      shortest(all_sequences)
+    end
+
+    @spec shortest(MapSet.t(move_sequence())) :: MapSet.t(move_sequence())
+    defp shortest(button_sequences) do
+      min_length = button_sequences |> Enum.map(&Enum.count/1) |> Enum.min()
+      button_sequences |>
+        Enum.filter(fn seq -> length(seq) == min_length end) |>
+        Enum.into(MapSet.new())
     end
 
     @spec shortest_direct_keypad_transitions() :: %{{direction_button(), direction_button()} => MapSet.t(move_sequence())}
-    defp shortest_direct_keypad_transition() do
+    defp shortest_direct_keypad_transitions() do
       %{
         {:a, :a} => MapSet.new([[]]),
         {:a, :^} => MapSet.new([[:<]]),
         {:a, :>} => MapSet.new([[:v]]),
         {:a, :v} => MapSet.new([[:v, :<], [:<, :v]]),
-        {:a, :<} => MapSet.new([[:v, :<, :<], [:<, :v, :<]]),
+        {:a, :<} => MapSet.new([[:v, :<, :<]]), #, [:<, :v, :<]]),
 
         {:^, :a} => MapSet.new([[:>]]),
         {:^, :^} => MapSet.new([[]]),
-        {:^, :>} => MapSet.new([[:v, :>], [:>, :v]]),
+        {:^, :>} => MapSet.new([[:>, :v], [:v, :>]]),
         {:^, :v} => MapSet.new([[:v]]),
-        {:^, :<} => MapSet.new([[:V, :<]]),
+        {:^, :<} => MapSet.new([[:v, :<]]),
 
         {:>, :a} => MapSet.new([[:^]]),
         {:>, :^} => MapSet.new([[:<, :^], [:^, :<]]),
@@ -87,7 +132,7 @@ defmodule Days.Day21 do
         {:v, :v} => MapSet.new([[]]),
         {:v, :<} => MapSet.new([[:<]]),
 
-        {:<, :a} => MapSet.new([[:>, :>, :^], [:>, :^, :>]]),
+        {:<, :a} => MapSet.new([[:>, :>, :^]]), #, [:>, :^, :>]]),
         {:<, :^} => MapSet.new([[:>, :^]]),
         {:<, :>} => MapSet.new([[:>, :>]]),
         {:<, :v} => MapSet.new([[:>]]),
@@ -233,13 +278,94 @@ defmodule Days.Day21 do
       }
     end
 
+    @spec complexity_enhanced(code(), pos_integer()) :: non_neg_integer()
+    defp complexity_enhanced(code, number_of_robots) do
+      number = number_part(code)
+      shortest_length = shortest_sequence_length_enhanced(code, number_of_robots)
+      number * shortest_length
+    end
 
-    @spec solve_part2(%Days.Day21{}, day_input()) :: String.t()
-    def solve_part2(_day_solver, input) do
-      (42 + String.length(to_string(input)) - String.length(to_string(input))) |>
-        to_string()
+    @spec shortest_sequence_length_enhanced(code(), pos_integer()) :: non_neg_integer()
+    defp shortest_sequence_length_enhanced(code, number_of_robots) do
+      digi_transitions = shortest_sensible_direct_digi_pad_transitions()
+      direction_transitions = shortest_direct_keypad_transitions()
+      first_directional_inputs = shortest_indirect_sequences(code, digi_transitions)
+      shortest_seq_lengths = first_directional_inputs |>
+        Enum.reduce(%{}, fn seq, known ->
+        shortest_sequence_length_enhanced(seq, number_of_robots, direction_transitions, known)
+      end)
+      first_directional_inputs |>
+        Enum.map(fn seq -> shortest_seq_lengths[{number_of_robots, seq}] end) |>
+        Enum.min()
+    end
+
+    @spec shortest_sequence_length_enhanced(move_sequence(), pos_integer(), %{{direction_button(), direction_button()} => MapSet.t(move_sequence())}, %{{non_neg_integer(), move_sequence()} => non_neg_integer()}) :: %{{non_neg_integer(), move_sequence()} => non_neg_integer()}
+    defp shortest_sequence_length_enhanced(button_sequence, number_of_robots, directional_transitions, known_lengths) do
+      if Map.has_key?(known_lengths, {number_of_robots, button_sequence}) do
+        known_lengths
+      else
+        if number_of_robots == 0 do
+          Map.put(known_lengths, {0, button_sequence}, length(button_sequence))
+        else
+          sub_seqs = button_sequence |>
+            Enum.chunk_while(
+              [],
+              fn button, reverse_buttons ->
+                if button == :a do
+                  {:cont, ([button] ++ reverse_buttons) |> Enum.reverse(), []}
+                else
+                  {:cont, [button] ++ reverse_buttons}
+                end
+              end,
+              fn reverse_buttons ->
+                if Enum.empty?(reverse_buttons) do
+                  {:cont, []}
+                else
+                  {:cont, reverse_buttons |> Enum.reverse(), []}
+                end
+              end
+            )
+          if length(sub_seqs) > 1 do
+            new_known = sub_seqs |>
+              Enum.reduce(known_lengths, fn seq, known ->
+                shortest_sequence_length_enhanced(seq, number_of_robots, directional_transitions, known)
+              end)
+            new_length = sub_seqs |>
+              Enum.map(fn seq -> new_known[{number_of_robots, seq}] end) |>
+              Enum.sum()
+            Map.put(new_known, {number_of_robots, button_sequence}, new_length)
+          else
+            # All shortest sequences are equally as valid; we take the first.
+            next_robot_seqs = shortest_indirect_sequences(button_sequence, directional_transitions)
+            new_known = next_robot_seqs |>
+              Enum.reduce(known_lengths, fn next_robot_seq, known ->
+                shortest_sequence_length_enhanced(next_robot_seq, number_of_robots - 1, directional_transitions, known)
+              end)
+            new_length = next_robot_seqs |>
+              Enum.map(fn seq -> new_known[{number_of_robots - 1, seq}] end) |>
+              Enum.min()
+            Map.put(new_known, {number_of_robots, button_sequence}, new_length)
+          end
+        end
+      end
     end
 
 
+
+    @spec solve_part1(%Days.Day21{}, day_input()) :: String.t()
+    def solve_part1(_day_solver, input) do
+      input |>
+        Enum.map(fn code -> complexity(code, 2) end) |>
+        Enum.sum() |>
+        to_string()
+    end
+
+    @spec solve_part2(%Days.Day21{}, day_input()) :: String.t()
+    def solve_part2(_day_solver, input) do
+      input |>
+        Enum.map(fn code -> complexity_enhanced(code, 25) end) |>
+        Enum.sum() |>
+        to_string()
+    end
   end
 end
--- Elixir/advent_of_code_2024/test/days/day21_test.exs
+++ Elixir/advent_of_code_2024/test/days/day21_test.exs
@@ -25,6 +25,61 @@ defmodule Day21Test do
     assert actual_input == expected_input
   end
 
+  test "day 21 part 1 works for sample input part 1", context do
+    input_string = ~s"029A"
+    expected_result = 68 * 29 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time part 1 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 1 works for sample input part 2", context do
+    input_string = ~s"980A"
+    expected_result = 60 * 980 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time part 2 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 1 works for sample input part 3", context do
+    input_string = ~s"179A"
+    expected_result = 68 * 179 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time part 3 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 1 works for sample input part 4", context do
+    input_string = ~s"456A"
+    expected_result = 64 * 456 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time part 4 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 1 works for sample input part 5", context do
+    input_string = ~s"379A"
+    expected_result = 64 * 379 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time part 5 in microseconds")
+    assert actual_result == expected_result
+  end
+
   test "day 21 part 1 works for sample input", context do
     input_string = ~s"029A
 980A
@@ -43,12 +98,83 @@ defmodule Day21Test do
   @tag timeout: :infinity
   test "day 21 part 1 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "171596"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
     {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
     IO.inspect(execution_time, label: "day #{context[:day]} part 1 real execution time in microseconds")
+    assert actual_result == expected_result
+  end
+
+
+
+
+
+
+  test "day 21 part 1 works for sample input part 1 ref", context do
+    input_string = ~s"029A"
+    expected_result = 68 * 29 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part1/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 1 example execution time part 1 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 2 works for sample input part 1", context do
+    input_string = ~s"029A"
+    expected_result = 82050061710 * 29 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time part 1 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 2 works for sample input part 2", context do
+    input_string = ~s"980A"
+    expected_result = 72242026390 * 980 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time part 2 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 2 works for sample input part 3", context do
+    input_string = ~s"179A"
+    expected_result = 81251039228 * 179 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time part 3 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 2 works for sample input part 4", context do
+    input_string = ~s"456A"
+    expected_result = 80786362258 * 456 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time part 4 in microseconds")
+    assert actual_result == expected_result
+  end
+
+  test "day 21 part 2 works for sample input part 5", context do
+    input_string = ~s"379A"
+    expected_result = 77985628636 * 379 |> to_string
+
+    solver = AdventOfCode2024.day_solver(context[:day])
+    input = DaySolver.parse_input(solver, input_string)
+    {execution_time, actual_result} = :timer.tc(&DaySolver.solve_part2/2, [solver, input])
+    IO.inspect(execution_time, label: "day #{context[:day]} part 2 example execution time part 5 in microseconds")
     assert actual_result == expected_result
   end
 
@@ -58,7 +184,7 @@ defmodule Day21Test do
 179A
 456A
 379A"
-    expected_result = "42"
+    expected_result = "154115708116294"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
@@ -70,7 +196,7 @@ defmodule Day21Test do
   @tag timeout: :infinity
   test "day 21 part 2 works for real input", context do
     input_string = InputReader.read_day_input(context[:day])
-    expected_result = "42"
+    expected_result = "209268004868246"
 
     solver = AdventOfCode2024.day_solver(context[:day])
     input = DaySolver.parse_input(solver, input_string)
